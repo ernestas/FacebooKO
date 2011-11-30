4f7a02d47cf7bba40e0489f183e9fa1fc592ad96
ernestas <ernestas.lisauskas@gmail.com>
2011-11-30 11:55:41 +0000

    change spaces to tabs

--- classes/facebooko/base.php
+++ classes/facebooko/base.php
@@ -2,21 +2,21 @@
 
 class FacebooKO_Base extends Facebook
 {
-    /**
-     * The factory method returns a Facebook instance. Usefull for chaining.
-     *
-     * @return  Facebook
-     */
-    public static function factory()
-    {
+	/**
+	* The factory method returns a Facebook instance. Usefull for chaining.
+	*
+	* @return  Facebook
+	*/
+	public static function factory()
+	{
 		$facebook = Kohana::$config->load('facebook');
 
-        return new FacebooKO(
-            array(
-                'appId'      => $facebook->app_id,
-                'secret'     => $facebook->secret,
-                'fileUpload' => $facebook->file_upload,
-            )
-        );
-    }
+		return new FacebooKO(
+			array(
+				'appId'      => $facebook->app_id,
+				'secret'     => $facebook->secret,
+				'fileUpload' => $facebook->file_upload,
+			)
+		);
+	}
 }
